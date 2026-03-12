b73db1b9c5fa56bca62e7bb9a6ace976d4d6a6f9
kymgriffins <kimutaigriffins1960@gmail.com>
2026-03-11 21:21:26 -0400

    fff

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,4 @@
 import os
 import sys
 
-# Add the project directory to Python path
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from bns.wsgi import application
